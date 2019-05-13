0bb0ded392f883ae986a614ad987f26b48ddb45b
Benjamin Dordoigne <benjamin.d@onthegosystems.com>
2019-05-13 08:07:14 +0100

    Added footer

---
+++ cp-2019-theme/canpromos/footer.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * The footer template.
+ *
+ * @package Avada
+ * @subpackage Templates
+ */
+
+// Do not allow directly accessing this file.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Direct script access denied.' );
+}
+?>
+					<?php do_action( 'avada_after_main_content' ); ?>
+
+				</div>  <!-- fusion-row -->
+			</main>  <!-- #main -->
+			<?php do_action( 'avada_after_main_container' ); ?>
+
+			<?php global $social_icons; ?>
+
+			<?php
+			/**
+			 * Get the correct page ID.
+			 */
+			$c_page_id = Avada()->fusion_library->get_page_id();
+			?>
+
+			<?php
+			/**
+			 * Only include the footer.
+			 */
+			?>
+			<?php if ( ! is_page_template( 'blank.php' ) ) : ?>
+				<?php $footer_parallax_class = ( 'footer_parallax_effect' === Avada()->settings->get( 'footer_special_effects' ) ) ? ' fusion-footer-parallax' : ''; ?>
+
+				<div class="fusion-footer<?php echo esc_attr( $footer_parallax_class ); ?>">
+					<?php get_template_part( 'templates/footer-content' ); ?>
+				</div> <!-- fusion-footer -->
+
+				<?php
+				/**
+				 * Add sliding bar.
+				 */
+				if ( Avada()->settings->get( 'slidingbar_widgets' ) ) {
+					get_template_part( 'sliding_bar' );
+				}
+				?>
+			<?php endif; // End is not blank page check. ?>
+		</div> <!-- wrapper -->
+
+		<?php
+		/**
+		 * Check if boxed side header layout is used; if so close the #boxed-wrapper container.
+		 */
+		$page_bg_layout = 'default';
+		if ( $c_page_id && is_numeric( $c_page_id ) ) {
+			$fpo_page_bg_layout = get_post_meta( $c_page_id, 'pyre_page_bg_layout', true );
+			$page_bg_layout     = ( $fpo_page_bg_layout ) ? $fpo_page_bg_layout : $page_bg_layout;
+		}
+		?>
+		<?php if ( ( ( 'Boxed' === Avada()->settings->get( 'layout' ) && 'default' === $page_bg_layout ) || 'boxed' === $page_bg_layout ) && 'Top' !== Avada()->settings->get( 'header_position' ) ) : ?>
+			</div> <!-- #boxed-wrapper -->
+		<?php endif; ?>
+		<?php if ( ( ( 'Boxed' === Avada()->settings->get( 'layout' ) && 'default' === $page_bg_layout ) || 'boxed' === $page_bg_layout ) && 'framed' === Avada()->settings->get( 'scroll_offset' ) && 0 !== intval( Avada()->settings->get( 'margin_offset', 'top' ) ) ) : ?>
+			<div class="fusion-top-frame"></div>
+			<div class="fusion-bottom-frame"></div>
+			<?php if ( 'None' !== Avada()->settings->get( 'boxed_modal_shadow' ) ) : ?>
+				<div class="fusion-boxed-shadow"></div>
+			<?php endif; ?>
+		<?php endif; ?>
+		<a class="fusion-one-page-text-link fusion-page-load-link"></a>
+		<!-- <table width="135" border="0" cellpadding="2" cellspacing="0" title="Click to Verify - This site chose Symantec SSL for secure e-commerce and confidential communications."><tr><td width="135" align="center" valign="top"><script type="text/javascript" src="https://seal.websecurity.norton.com/getseal?host_name=https://www.canpromos.ca&amp;size=L&amp;use_flash=NO&amp;use_transparent=No&amp;lang=en"></script><br /><a href="https://www.websecurity.symantec.com/ssl-certificate" target="_blank"  style="color:#000000; text-decoration:none; font:bold 10px verdana,sans-serif; letter-spacing:.5px;text-align:center; margin:0px; padding:0px;"> ABOUT SSL CERTIFICATES</a></td></tr></table> -->
+		<?php wp_footer(); ?>
+	</body>
+</html>
